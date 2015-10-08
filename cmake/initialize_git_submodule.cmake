4c2d19158abd14ed7657c2ad1cc2e83d57d24357
BJörn <bjorn.nutti@gmail.com>
2015-10-08 21:53:15 +0200

    * Added initial version of automatic submodule branch selection via CMake * Added example projects that uses different branches of the msp-lib repository

--- cmake/initialize_git_submodule.cmake
+++ cmake/initialize_git_submodule.cmake
@@ -1,5 +1,16 @@
 ##
+#
+##
+function(initialize_all_submodules)
+execute_process(COMMAND             git submodule update --init --recursive
+                WORKING_DIRECTORY   ${CMAKE_CURRENT_SOURCE_DIR}
+)
+endfunction(initialize_all_submodules)
+
+##
 # Initializes GIT submodules
+# @NAME - name of local directory where the submodule is stored
+# @BRANCH - name of submodule branch
 ##
 function(initialize_git_submodule)
 
@@ -9,25 +20,12 @@ set(multiValueArgs)
 include(CMakeParseArguments)
 cmake_parse_arguments(RHS  "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-set(PROJECT_SOURCE_DIR  ${CMAKE_SOURCE_DIR})
-if(EXISTS "${PROJECT_SOURCE_DIR}/.gitmodules")
 message(STATUS "Updating submodule ${RHS_NAME} : [${RHS_BRANCH}]")
 message(STATUS "(this can take a while, please be patient)")
 
-### First, get all submodules in
-execute_process(COMMAND             git submodule update --init --recursive
-                WORKING_DIRECTORY   ${PROJECT_SOURCE_DIR}
-)
-
-### Then, checkout each submodule to the specified commit
-# Note: Execute separate processes here, to make sure each one is run,
-# should one crash (because of branch not existing, this, that ... whatever)
-message(STATUS "git checkout ${RHS_BRANCH} in ${PROJECT_SOURCE_DIR}/${RHS_SUBDIRECTORY}/${RHS_NAME}")
+# Checkout each submodule to the specified branch
+message(STATUS "git checkout ${RHS_BRANCH} in ${CMAKE_CURRENT_SOURCE_DIR}/${RHS_SUBDIRECTORY}/${RHS_NAME}")
 execute_process(COMMAND git checkout ${RHS_BRANCH}
-                WORKING_DIRECTORY    ${PROJECT_SOURCE_DIR}/${RHS_SUBDIRECTORY}/${RHS_NAME}
+                WORKING_DIRECTORY    ${CMAKE_CURRENT_SOURCE_DIR}/${RHS_SUBDIRECTORY}/${RHS_NAME}
 )
-else()
-  message(WARNING "Git repo is not setup correctly")
-endif()
-
 endfunction(initialize_git_submodule)
---
+++ easy-angle-3.3/pc/readme.txt
@@ -0,0 +1,1 @@
+pc applications goes in here
